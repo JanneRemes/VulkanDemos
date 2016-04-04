6a9951c7a71c102bdc3dd0b2401bf91126cb71ae
Z80Fan <z80fan@altervista.org>
2016-04-04 22:06:21 +0200

    Changed image initialization in demo 01 and 02 to execute a single pipeline barrier

--- 01_clearscreen/main.cpp
+++ 01_clearscreen/main.cpp
@@ -321,9 +321,40 @@ int main(int argc, char* argv[])
 bool fillInitializationCommandBuffer(const VkCommandBuffer theCommandBuffer, const std::vector<VkImage> & theSwapchainImagesVector)
 {
 	VkResult result;
+	std::vector<VkImageMemoryBarrier> memoryBarriersVector;
 
 	/*
-	 * Begin recording of the command buffer
+	 * "The pipeline barrier specifies an execution dependency such that all work performed
+	 * by the set of pipeline stages included in srcStageMask of the first set of commands
+	 * completes before any work performed by the set of pipeline stages
+	 * included in dstStageMask of the second set of commands begins." [Section 6.5]
+	 *
+	 * Pipeline barriers are also the place were we can change layouts of our VkImages;
+	 * here we add a Pipeline Barrier for each swapchain image, to transition them
+	 * from VK_IMAGE_LAYOUT_UNDEFINED to the correct layout.
+	*/
+	for(const auto & image : theSwapchainImagesVector)
+	{
+		VkImageMemoryBarrier imageMemoryBarrier = {
+			.sType = VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER,
+			.pNext = nullptr,
+			.srcAccessMask = 0,
+			.dstAccessMask = 0,
+			.oldLayout = VK_IMAGE_LAYOUT_UNDEFINED,
+			.newLayout = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR,
+			.srcQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED,
+			.dstQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED,
+			.subresourceRange = {VK_IMAGE_ASPECT_COLOR_BIT, 0, 1, 0, 1},
+			.image = image,
+		};
+
+		memoryBarriersVector.push_back(imageMemoryBarrier);
+	}
+
+
+	/*
+	 * Now we submit all the Pipeline Barriers to a command buffer
+	 * through the vkCmdPipelineBarrier command.
 	 */
 	VkCommandBufferBeginInfo commandBufferBeginInfo = {
 		.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO,
@@ -332,51 +363,24 @@ bool fillInitializationCommandBuffer(const VkCommandBuffer theCommandBuffer, con
 		.pInheritanceInfo = nullptr,
 	};
 
+	// Begin command buffer recording.
 	result = vkBeginCommandBuffer(theCommandBuffer, &commandBufferBeginInfo);
 	assert(result == VK_SUCCESS);
 
-	/*
-	 * "The pipeline barrier specifies an execution dependency such that all work performed
-	 * by the set of pipeline stages included in srcStageMask of the first set of commands
-	 * completes before any work performed by the set of pipeline stages
-	 * included in dstStageMask of the second set of commands begins." [Section 6.5]
-	*/
-	VkImageMemoryBarrier imageMemoryBarrier = {
-		.sType = VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER,
-		.pNext = nullptr,
-		.srcAccessMask = 0,
-		.dstAccessMask = 0,
-		.oldLayout = VK_IMAGE_LAYOUT_UNDEFINED,
-		.newLayout = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR,
-		.srcQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED,
-		.dstQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED,
-		.subresourceRange = {VK_IMAGE_ASPECT_COLOR_BIT, 0, 1, 0, 1}
-	};
+	// Submit the Pipeline Barriers
+	vkCmdPipelineBarrier(theCommandBuffer,
+		VK_PIPELINE_STAGE_TOP_OF_PIPE_BIT,     // srcStageMask
+		VK_PIPELINE_STAGE_TOP_OF_PIPE_BIT,     // dstStageMask
+		0,                                     // dependencyFlags
+		0,                                     // memoryBarrierCount
+		nullptr,                               // pMemoryBarriers
+		0,                                     // bufferMemoryBarrierCount
+		nullptr,                               // pBufferMemoryBarriers
+		(uint32_t)memoryBarriersVector.size(), // imageMemoryBarrierCount
+		memoryBarriersVector.data()            // pImageMemoryBarriers
+	);
 
-	VkPipelineStageFlags srcStageMask = VK_PIPELINE_STAGE_TOP_OF_PIPE_BIT;	// TODO explain those bits
-	VkPipelineStageFlags dstStageMask = VK_PIPELINE_STAGE_TOP_OF_PIPE_BIT;
-
-	/*
-	 * Add a PipelineBarrier command for each swapchain image.
-	 */
-	for(const auto & image : theSwapchainImagesVector)
-	{
-		imageMemoryBarrier.image = image;
-
-		vkCmdPipelineBarrier(theCommandBuffer, srcStageMask, dstStageMask,
-			0,         // dependencyFlags
-			0,         // memoryBarrierCount
-			nullptr,   // pMemoryBarriers
-			0,         // bufferMemoryBarrierCount
-			nullptr,   // pBufferMemoryBarriers
-			1,         // imageMemoryBarrierCount
-			&imageMemoryBarrier // pImageMemoryBarriers
-		);
-	}
-
-	/*
-	 * End recording of the command buffer
-	 */
+	// End command buffer recording.
 	result = vkEndCommandBuffer(theCommandBuffer);
 	return true;
 }
